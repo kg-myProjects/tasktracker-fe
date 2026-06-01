9402793d04e21e5520581afffb3046922ebab806
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-01 16:46:33 +0200

    minor formatting vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    open: true,
-
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/",
-        changeOrigin: true,
-      },
+    plugins: [react(), tailwindcss()],
+    server: {
+        open: true,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080/",
+                changeOrigin: true,
+            },
+        },
     },
-  },
 });
