ab84636ea9266a0cc5fbae3b2d89b23358b0abd9
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-13 23:50:41 +0200

    ui: redesign invite modal

--- src/features/projects/components/InviteModal.tsx
+++ src/features/projects/components/InviteModal.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import {useState} from "react";
 import type {InviteModalProps, ProjectRole} from "../types";
+import PulsedStripe from "../../../components/ui/PulsedStripe.tsx";
+import MainButton from "../../../components/ui/buttons/MainButton.tsx";
 
-export function InviteModal({ isOpen, onClose, onInvite, error }: InviteModalProps) {
+export function InviteModal({isOpen, onClose, onInvite, error}: InviteModalProps) {
     const [email, setEmail] = useState("");
     const [role, setRole] = useState<ProjectRole>("MEMBER");
 
@@ -16,45 +18,44 @@ export function InviteModal({ isOpen, onClose, onInvite, error }: InviteModalPro
     };
 
     return (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-[100]">
-            <div className="bg-white p-6 rounded-lg shadow-xl w-96 text-black">
-                <h2 className="text-xl font-bold mb-4">Invite Collaborator</h2>
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50"
+             onClick={onClose}>
+            <div className="bg-slate-900 w-96 text-white border border-cyan-500/20 hover:border-cyan-500/40 rounded-xl"
+                 onClick={(e) => e.stopPropagation()}>
+                <div className="bg-gradient-to-r from-slate-900 via-cyan-950 to-slate-900 rounded-xl">
+                    <h2 className="h-20 flex items-center justify-center text-xl font-bold text-white text-neon-strong uppercase">
+                        Invite collaborator
+                    </h2>
+                    <PulsedStripe height="2px"></PulsedStripe>
+                </div>
 
-                <input
-                    type="email"
-                    placeholder="User email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="w-full border p-2 rounded mb-4 focus:ring-2 focus:ring-cyan-400 outline-none"
-                />
-
-                <select
-                    value={role}
-                    onChange={(e) => setRole(e.target.value as ProjectRole)}
-                    className="w-full border p-2 rounded mb-4"
-                >
-                    <option value="MEMBER">Member</option>
-                    <option value="ADMIN">Admin</option>
-                    <option value="VIEWER">Viewer</option>
-                </select>
-
-                {error && <p className="text-red-500 text-sm mb-4 font-medium">{error}</p>}
-
-                <div className="flex justify-end gap-2">
-                    <button
-                        type="button"
-                        onClick={onClose}
-                        className="px-4 py-2 border rounded hover:bg-gray-100 transition-colors"
+                <div className="p-4">
+                    <input
+                        type="email"
+                        placeholder="User email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-full px-3 py-2 mb-3 border border-cyan-500/20 hover:border-cyan-500/40 focus:outline-none focus:border-cyan-500/40 rounded-xl"
+                    />
+                    <select
+                        value={role}
+                        onChange={(e) => setRole(e.target.value as ProjectRole)}
+                        className="w-full px-3 py-2 mb-3 border border-cyan-500/20 hover:border-cyan-500/40 focus:outline-none focus:border-cyan-500/40 rounded-xl"
                     >
-                        Cancel
-                    </button>
-                    <button
-                        type="button"
-                        onClick={handleInviteClick}
-                        className="px-4 py-2 bg-cyan-400 text-white rounded hover:bg-cyan-500 transition-colors"
-                    >
-                        Invite
-                    </button>
+                        <option value="MEMBER" className="bg-slate-900 text-white">Member</option>
+                        <option value="ADMIN" className="bg-slate-900 text-white">Admin</option>
+                        <option value="VIEWER" className="bg-slate-900 text-white">Viewer</option>
+                    </select>
+                    {error && <p className="text-red-500 text-sm mb-4 font-medium">{error}</p>}
+                    <div className="flex justify-end">
+                        <MainButton
+                            type="button"
+                            variant="primary"
+                            onClick={handleInviteClick}
+                        >
+                            Invite
+                        </MainButton>
+                    </div>
                 </div>
             </div>
         </div>
