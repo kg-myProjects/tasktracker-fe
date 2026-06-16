9c196ccf3d0ccfd5fa54245fcf381693d79b2253
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-16 23:31:06 +0200

    ui:header refactor and start to adaptive resp

--- src/components/ui/buttons/MainButton.tsx
+++ src/components/ui/buttons/MainButton.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 type ButtonVariant = "primary" | "danger";
 
-interface MainButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+type MainButtonProps = {
     variant?: ButtonVariant;
     width?: number;
-}
+    to?: string;
+    children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function MainButton({children, variant = "primary", width, className = "", ...props}: MainButtonProps) {
+export default function MainButton({children, variant = "primary", width, className = "", to, ...props}: MainButtonProps) {
 
-    const baseStyles = "rounded-xl px-6 py-2.5 text-sm font-black text-white transition-all duration-300 ease-in-out hover:scale-[1.20]";
+    const baseStyles =
+        "rounded-xl px-6 py-2.5 text-sm font-black text-white transition-all duration-300 ease-in-out hover:scale-[1.20]";
 
     const variantStyles = {
         primary:
@@ -19,13 +22,18 @@ export default function MainButton({children, variant = "primary", width, classN
             "bg-red-500 border border-red-300/50 shadow-[0_0_20px_rgba(239,68,68,0.6)] hover:bg-red-700 hover:shadow-[0_0_35px_rgba(239,68,68,0.9)]",
     };
 
+    const classes = `${baseStyles} ${variantStyles[variant]} ${className}`;
+
+    if (to) {
+        return (
+            <Link to={to} className={classes}>
+                {children}
+            </Link>
+        );
+    }
+
     return (
-        <button
-            type={props.type ?? "button"}
-            className={`${baseStyles} ${variantStyles[variant]} ${className}`}
-            style={width ? { width: `${width}px` } : undefined}
-            {...props}
-        >
+        <button type={props.type ?? "button"} className={classes} style={width ? {width: `${width}px`} : undefined}{...props}>
             {children}
         </button>
     );
