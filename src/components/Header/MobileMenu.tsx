3317ea1351e44be0b2de565eae89f3b4f0990af6
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-22 23:35:17 +0200

    ui:profile start to adaptive response

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -1,7 +1,7 @@
-import {Link} from "react-router-dom";
 import MainButton from "../ui/buttons/MainButton";
 import {useAppDispatch} from "../../app/hooks";
 import {logout} from "../../features/auth/slice/authSlice";
+import NavigationLink from "./NavigationLink.tsx";
 
 type Props = {
     open: boolean;
@@ -20,13 +20,15 @@ export default function MobileMenu({open, onClose}: Props) {
         <div className={`fixed inset-0 z-50 transition-all duration-300 ${open ? "visible opacity-100" : "invisible opacity-0"}`}>
             <div className="absolute inset-0 bg-black/60" onClick={onClose}/>
             <div
-                className={`absolute right-0 top-0 h-full w-50 bg-slate-900 border-l-2 border-cyan-500/20 p-6 transform transition-transform duration-300 ${
+                className={`absolute right-0 top-0 h-full w-50 bg-slate-900 border-l-2 border-cyan-500/20 p-11 transform transition-transform duration-300 ${
                     open ? "translate-x-0" : "translate-x-full"
                 }`}
             >
-                <div className="flex flex-col gap-6 text-white">
-                    <Link to="/about" onClick={onClose}>About</Link>
-                    <Link to="/projects" onClick={onClose}>My boards</Link>
+                <div className="flex flex-col gap-4">
+                    <NavigationLink to="/about" onClick={onClose}>About</NavigationLink>
+                    <NavigationLink to="/projects" onClick={onClose}>My boards</NavigationLink>
+                    <NavigationLink to="/profile" onClick={onClose}>Profile</NavigationLink>
+                    <hr className="border-cyan-500/20"/>
                     <MainButton onClick={handleLogout}>
                         Logout
                     </MainButton>
--- src/components/Header/NavigationLink.tsx
+++ src/components/Header/NavigationLink.tsx
@@ -1,18 +1,21 @@
 import {NavLink} from "react-router-dom";
+import React from "react";
 
 interface NavigationLinkProps {
     to: string;
     children: React.ReactNode;
     className?: string;
+    onClick?: () => void;
 }
 
-export default function NavigationLink({to, children, className = "",}: NavigationLinkProps) {
+export default function NavigationLink({to, children, className = "", onClick,}: NavigationLinkProps) {
     return (
         <NavLink
             to={to}
+            onClick={onClick}
             className={({ isActive }) => `text-sm font-bold whitespace-nowrap transition-all duration-300 ease-in-out 
             ${isActive
-                    ? "text-cyan-400 scale-105 drop-shadow-[0_0_15px_rgba(6,182,212,1)] text-shadow-[0_0_10px_rgba(6,182,212,0.8)]"
+                    ? "text-cyan-400 drop-shadow-[0_0_15px_rgba(6,182,212,1)] text-shadow-[0_0_10px_rgba(6,182,212,0.8)]"
                     : "text-white"
             }
                 hover:text-cyan-400
