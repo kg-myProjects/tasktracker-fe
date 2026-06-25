c16631beebc52cb7b4fb8a37d5a3964c56298211
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-25 03:06:05 +0200

    fix: small typo-21

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -283,7 +283,7 @@ export default function Profile() {
                                             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                                                 setter(e.target.value)
                                             }
-                                            className="flex-1 w-full px-1 py-[1px] md:py-1 rounded border border-cyan-300 bg-slate-800 text-white"
+                                            className="flex-1 w-full px-1 py-[1px] md:py-1 mr-4 rounded border border-cyan-300 bg-slate-800 text-white"
                                         />
                                     ) : (
                                         <span
