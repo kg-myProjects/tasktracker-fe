770f8d217e7a05eab41f38b140eaa089bb38c079
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2026-06-25 02:33:45 +0200

    fix: small typo-10

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -274,7 +274,7 @@ export default function Profile() {
                         {/* Left info block */}
                         <div className="flex-1 space-y-1 md:space-y-4">
                             {fields.map(([label, value, setter]) => (
-                                <div key={label as string} className="flex items-center gap-2">
+                                <div key={label as string} className="flex items-center gap-4">
                                     <label className="w-18 md:w-32 text-xs md:text-base font-bold text-cyan-300 whitespace-nowrap">{label}:</label>
                                     {isEditingProfile ? (
                                         <input
